fba1f84f5dd9a8fb03595f7983780defb9e139b1
Greg Burkett <greg@ripfish.com>
2014-05-27 12:16:25 -0400

    Reseting my boo-boo

--- themes/ridewalk/lib/scripts.php
+++ themes/ridewalk/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c615e795056758df8f91f82dc358c288');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '61b492121e522a07c6c80ef2a3e7bcea');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
