c755d196d25aafa67ac8e53f45d6e04c0fb52844
Greg Burkett <greg@ripfish.com>
2014-05-28 10:52:55 -0400

    lots of blog and style fixes, removing author byline

--- themes/ridewalk/lib/scripts.php
+++ themes/ridewalk/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '873f2cc02cc4a2b06c3222730be25b1c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ec6290770fb83083b17e04482592b920');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
