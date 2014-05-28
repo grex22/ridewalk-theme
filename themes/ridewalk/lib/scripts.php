43cee1ea063fc00b7d2d36f682ef33461764c9df
Greg Burkett <greg@ripfish.com>
2014-05-28 12:18:06 -0400

    content fixes

--- themes/ridewalk/lib/scripts.php
+++ themes/ridewalk/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9adf87668c2d1da29a943aa3459ca4da');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '181edc6ecf7245f371ee2ecc96569252');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
