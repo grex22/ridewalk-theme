5d5328325755f71ad7df59141cbb668c3428d3e7
Greg Burkett <greg@ripfish.com>
2014-05-27 20:33:55 -0400

    coord adjust

--- themes/ridewalk/lib/scripts.php
+++ themes/ridewalk/lib/scripts.php
@@ -30,7 +30,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', array(), null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), 'b94fd638a063be980fd1af9d7223906e', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), 'd7285352f4eaf0b9616048632b75112d', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('googlemaps');
