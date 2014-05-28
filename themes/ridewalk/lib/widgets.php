6c519d864214461320dd727308f691b0e1e32186
Greg Burkett <greg@ripfish.com>
2014-05-28 10:04:58 -0400

    home sidebar for events

--- themes/ridewalk/lib/widgets.php
+++ themes/ridewalk/lib/widgets.php
@@ -21,6 +21,15 @@ function roots_widgets_init() {
     'before_title'  => '<h3>',
     'after_title'   => '</h3>',
   ));
+  
+  register_sidebar(array(
+    'name'          => __('Home Sidebar', 'roots'),
+    'id'            => 'sidebar-home',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h2>',
+    'after_title'   => '</h2>',
+  ));
 
   // Widgets
   register_widget('Roots_Vcard_Widget');
