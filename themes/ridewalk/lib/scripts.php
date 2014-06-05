b1f25b17964f0e47599a45f86473c32d74910b7b
Greg Burkett <greg@ripfish.com>
2014-06-05 11:29:41 -0400

    hr clearfloat

--- themes/ridewalk/lib/scripts.php
+++ themes/ridewalk/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a06427c8ab7a3a54a566185759094a70');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6857bb1b33c9f722e5ea2aca0a1f540d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
