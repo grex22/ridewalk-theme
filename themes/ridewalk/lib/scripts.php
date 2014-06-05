a59fa72016ada26561195217ad360ccfbe2b4345
Greg Burkett <greg@ripfish.com>
2014-06-05 11:32:52 -0400

    fix hr clearfloat

--- themes/ridewalk/lib/scripts.php
+++ themes/ridewalk/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6857bb1b33c9f722e5ea2aca0a1f540d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e349bf2e9d5f9b8513a975dde7fd206a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
