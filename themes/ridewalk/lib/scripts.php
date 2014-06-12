07a87d0d41c48a271cbbfb480c3a3a48dae7989a
Greg Burkett <greg@ripfish.com>
2014-06-12 12:11:03 -0400

    Tweak to dropdown menu font size

--- themes/ridewalk/lib/scripts.php
+++ themes/ridewalk/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e349bf2e9d5f9b8513a975dde7fd206a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '673ff12b8da72a5469f0205a0598bd91');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
