833bd5fc320b0fbf11ce56b020ccc7fff9717a93
Greg Burkett <greg@ripfish.com>
2014-06-17 13:57:25 -0400

    homepage title change

--- themes/ridewalk/templates/head.php
+++ themes/ridewalk/templates/head.php
@@ -3,7 +3,14 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?php wp_title('|', true, 'right'); ?></title>
+  <title><?php wp_title('|', true, 'right');
+  
+  if(is_front_page()):
+    echo " &rsaquo; ";
+    bloginfo('description');
+  endif;
+  
+  ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <?php wp_head(); ?>
